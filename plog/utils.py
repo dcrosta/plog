5e1804d98ac460c1abddb34844cbfe676e375fa2
Dan Crosta <dcrosta@gmail.com>
2011-09-18 09:14:06 -0400

    add cache-control headers

--- plog/utils.py
+++ plog/utils.py
@@ -1,6 +1,9 @@
 __all__ = ('get_or_404',)
 
-from flask import abort
+from datetime import datetime, timedelta
+
+from plog import app
+from flask import abort, request
 
 def get_or_404(cls, **kwargs):
     obj = cls.objects(**kwargs).first()
@@ -8,3 +11,11 @@ def get_or_404(cls, **kwargs):
         abort(404)
     return obj
 
+@app.after_request
+def set_cache_headers(response):
+    if not request.path.startswith('/admin'):
+        response.cache_control.public = True
+        response.cache_control.max_age = 6 * 60 * 60
+    if response.expires is None:
+        response.expires = datetime.utcnow() + timedelta(hours=6)
+    return response
